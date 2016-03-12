e3384a1cecc52feee19d9c86b8ca0c6613777eec
Yuan Gong <13761657302@163.com>
2016-03-12 09:04:31 -0500

    Adding the wear recording function version drei

--- wear/src/main/java/com/example/kyle/yuanapp2/MainActivity.java
+++ wear/src/main/java/com/example/kyle/yuanapp2/MainActivity.java
@@ -1,24 +1,79 @@
 package com.example.kyle.yuanapp2;
 
 import android.app.Activity;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+    Button play,stop,record;
+    private MediaRecorder myAudioRecorder;
+    private String outputFile = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+        setContentView(R.layout.round_activity_main);
+
+        play = (Button) findViewById(R.id.play);
+        stop = (Button) findViewById(R.id.stop);
+        record = (Button) findViewById(R.id.start);
+
+        stop.setEnabled(false);
+        play.setEnabled(false);
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+        ;
+
+        myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+      //  myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_WB);
+      //  myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.DEFAULT);
+        myAudioRecorder.setOutputFile(outputFile);
+
+        record.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+            public void onClick(View v) {
+                try {
+                    myAudioRecorder.prepare();
+                    myAudioRecorder.start();
+                } catch (IllegalStateException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                record.setEnabled(false);
+                stop.setEnabled(true);
+                Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
             }
         });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myAudioRecorder.stop();
+                myAudioRecorder.release();
+                myAudioRecorder = null;
+
+                stop.setEnabled(false);
+                play.setEnabled(true);
+
+                Toast.makeText(getApplicationContext(), "Audio recorded successfully",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+
     }
 }
